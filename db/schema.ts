ae7760f502d2cdb2272140e96677e7c4a356511b
Dylan Palmboom <nofxboy1234@gmail.com>
2026-09-02 14:21:13 +0200

    Update formatting config

--- db/schema.ts
+++ db/schema.ts
@@ -4,5 +4,7 @@ import { integer, sqliteTable, text } from "void/schema-d1";
 export const messages = sqliteTable("messages", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   text: text("text").notNull(),
-  createdAt: text("created_at").notNull().default(sql`(datetime('now'))`),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`(datetime('now'))`),
 });
